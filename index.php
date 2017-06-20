b404d8ae557ed6a73cb9bda28736e66e866a7f42
Lan4077 <Lan4077@yandex.ru>
2017-06-20 20:26:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?
-
-$client_id = '1111111';
-$scope = 'offline,messages'
+<?php
+echo "Hello, world!";
 ?>
-
-<a href="https://oauth.vk.com/authorize?client_id=<?=$client_id;?>&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=<?=$scope;?>&response_type=token&v=5.37">Push the button</a>
